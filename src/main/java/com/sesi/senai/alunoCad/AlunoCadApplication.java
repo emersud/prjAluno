15d165bd42ef1fbf5a6883c4d0ff39dd09123617
emersud <emersud@gmail.com>
2023-09-09 14:12:13 -0300

    Creiação do Projeto

---
+++ src/main/java/com/sesi/senai/alunoCad/AlunoCadApplication.java
@@ -0,0 +1,13 @@
+package com.sesi.senai.alunoCad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoCadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoCadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesi/senai/alunoCad/AlunoCadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesi.senai.alunoCad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoCadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
